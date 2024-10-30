ff78496d372c458ecdcd75d2e9893ac5438f6327
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-10-30 22:56:24 +0500

    docs(date): Добавлены докстринги и тайп хинты

--- app/decorator.py
+++ app/decorator.py
@@ -1,21 +1,18 @@
 from app.interfaces import IMessage
-from .message import Message
-
 
 class MessageDecorator(IMessage):
     """
     Базовый класс для декораторов сообщений
     """
 
-    def __init__(self, message: IMessage):
-        self._message = message
+    def __init__(self, message: IMessage) -> None:
+        """
+        Иницаилизация декоратора сообщения
+        """
+        self._message: IMessage = message
 
-    def Print(self):
-        self._message.Print()
-
-    def get_content(self):
-        """Возвращает контент сообщения."""
-        if isinstance(self._message, Message):
-            return self._message.content
-        else:
-            return self._message.get_content()
+    def print(self) -> None:
+        """
+        Печатает сообщение
+        """
+        self._message.print()
