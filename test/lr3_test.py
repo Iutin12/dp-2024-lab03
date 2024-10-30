01e1458e9248f324937bc28bb1d21b592fb5d6c6
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-10-30 23:01:14 +0500

    fix(lr3_test): вызов методов сделал с маленькой буквы

--- test/lr3_test.py
+++ test/lr3_test.py
@@ -3,51 +3,58 @@ import io
 import base64
 from contextlib import redirect_stdout
 from datetime import datetime
-from app.header import HeaderPrint
-from app.footer import FooterPrint
-from app.date import DatePrint
-from app.base64 import Base64Print
+from app.header import HeaderDecorator
+from app.footer import FooterDecorator
+from app.date import DateDecorator
+from app.base64 import Base64Decorator
 from app.message import Message
+
 class TestMessageDecorators(unittest.TestCase):
 
     def setUp(self):
+        """Создание начального сообщения"""
         self.message = Message("С наступающим Новым годом!")
 
     def test_message_print(self):
+        """Тест печати начльного сообщения"""
         with io.StringIO() as buf, redirect_stdout(buf):
-            self.message.Print()
+            self.message.print()
             output = buf.getvalue().strip()
         self.assertEqual(output, "С наступающим Новым годом!")
 
     def test_header_decorator(self):
+        """Тест декоратора заголовка"""
         header = "Добрый день,"
-        decorated_message = HeaderPrint(self.message, header)
+        decorated_message = HeaderDecorator(self.message, header)
         with io.StringIO() as buf, redirect_stdout(buf):
-            decorated_message.Print()
+            decorated_message.print()
             output = buf.getvalue().strip()
         self.assertEqual(output, f"{header}\nС наступающим Новым годом!")
 
     def test_footer_decorator(self):
+        """Тест декорратора подписи"""
         footer = "Дед Мороз"
-        decorated_message = FooterPrint(self.message, footer)
+        decorated_message = FooterDecorator(self.message, footer)
         with io.StringIO() as buf, redirect_stdout(buf):
-            decorated_message.Print()
+            decorated_message.print()
             output = buf.getvalue().strip()
         self.assertEqual(output, "С наступающим Новым годом!\nДед Мороз")
 
     def test_date_decorator(self):
+        """Тест декоратора даты"""
         date_str = datetime(2020, 12, 26).strftime("%d.%m.%Y")
-        decorated_message = DatePrint(self.message, date_str)
+        decorated_message = DateDecorator(self.message, date_str)
         with io.StringIO() as buf, redirect_stdout(buf):
-            decorated_message.Print()
+            decorated_message.print()
             output = buf.getvalue().strip()
         self.assertEqual(output, "С наступающим Новым годом!\n" + date_str)
 
     def test_base64_decorator(self):
-        decorated_message = Base64Print(self.message)
+        """Тест декоратора для кодирования"""
+        decorated_message = Base64Decorator(self.message)
         expected_base64 = base64.b64encode(self.message.content.encode()).decode()
         with io.StringIO() as buf, redirect_stdout(buf):
-            decorated_message.Print()
+            decorated_message.print()
             output = buf.getvalue().strip()
         self.assertEqual(output, expected_base64)
 
