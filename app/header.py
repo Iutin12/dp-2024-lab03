34f512f0f9fdfa4d44219619a0b41efdca6e15ef
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-10-31 20:11:12 +0500

    fix(header): добавлен новый метод

--- app/header.py
+++ app/header.py
@@ -1,6 +1,7 @@
 from app.interfaces import IMessage
 from .decorator import MessageDecorator
 
+
 class HeaderDecorator(MessageDecorator):
     """
     Декоратор для добавления заголовка к сообщению
@@ -11,11 +12,17 @@ class HeaderDecorator(MessageDecorator):
         Инициализация декоратора для заголовка
         """
         super().__init__(message)
-        self.header: str = header
+        self._header: str = header
 
     def print(self) -> None:
         """
         Печатает заголовок и сообщение
         """
-        print(self.header)
+        print(self._header)
         super().print()
+
+    def get_content(self) -> str:
+        """
+        Возвращает содержимое сообщения с добавленным заголовком
+        """
+        return f"{self._header}\n{self._message.get_content()}"
